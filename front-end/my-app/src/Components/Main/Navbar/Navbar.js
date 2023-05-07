1892d1cbf9300083a9b0b42319e404b9354f5ef6
James <jkeezer@pm.me>
2023-05-06 22:37:05 -0400

    Navbar for desktop (active link not yet working)

--- front-end/my-app/src/Components/Main/Navbar/Navbar.js
+++ front-end/my-app/src/Components/Main/Navbar/Navbar.js
@@ -1,14 +1,61 @@
-import { useState, useEffect } from "react";
-
-import "./Navbar.css";
+import React, { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
-      <p>Logo</p>
-      <a>Search</a>
-      <a>Create</a>
-      <a>Profile</a>
+    <div className={styles.Navbar}>
+      <ul>
+        <NavLink to="/" className="navLink">
+          <li>
+            <Icon icon="ion:home" className={styles.navIcon} />
+            <p>Home</p>
+          </li>
+        </NavLink>
+        <NavLink to="/search" className="navLink">
+        <li>
+          <Icon icon="ion:search" className={styles.navIcon} />
+          <p>Search</p>
+        </li>
+        </NavLink>
+        <NavLink to="/explore" className="navLink">
+        <li>
+          <Icon icon="ion:compass-outline" className={styles.navIcon} />
+          <p>Explore</p>
+        </li>
+        </NavLink>
+        <NavLink to="/reels" className="navLink">
+        <li>
+          <Icon icon="ion:heart-outline" className={styles.navIcon} />
+          <p>Reels</p>
+        </li>
+        </NavLink>
+        <NavLink to="/messages" className="navLink">
+        <li>
+          <Icon icon="ion:paper-plane-outline" className={styles.navIcon} />
+          <p>Messages</p>
+        </li>
+        </NavLink>
+        <NavLink to="/notifications" className="navLink">
+        <li>
+          <Icon icon="ion:heart-outline" className={styles.navIcon} />
+          <p>Notifications</p>
+        </li>
+        </NavLink>
+        <NavLink to="/create" className="navLink">
+        <li>
+          <Icon icon="ion:add-circle-outline" className={styles.navIcon} />
+          <p>Create</p>
+        </li>
+        </NavLink>
+        <NavLink to="/profile" className="navLink">
+        <li>
+          <Icon icon="ion:person-circle" className={styles.navIcon} />
+          <p>Profile</p>
+        </li>
+        </NavLink>
+      </ul>
     </div>
   );
 };
